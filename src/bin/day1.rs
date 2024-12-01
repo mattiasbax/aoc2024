eae7398a96fb433dd0481ad6141b8f95351a3c3f
Mattias Bäckström <mattias.bax@live.se>
2024-12-01 14:05:57 +0100

    day1 b

--- src/bin/day1.rs
+++ src/bin/day1.rs
@@ -1,26 +1,54 @@
+use std::collections::HashMap;
+
 const INPUT: &str = include_str!("../../input/day1.txt");
 
-pub fn day1a() {
-    let lines = INPUT.lines();
-
-    let mut vec1 = Vec::new();
-    let mut vec2 = Vec::new();
-
-    for line in lines {
-        let (part1, part2) = line.split_once(" ").unwrap();
-        let num1 = part1.parse::<i32>().unwrap();
-        vec1.push(num1);
-        let num2 = part2.parse::<i32>().unwrap();
-        vec2.push(num2);
-    }
+fn day1a() -> i32 {
+    let (mut vec1, mut vec2): (Vec<_>, Vec<_>) = INPUT
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(part1, part2)| (part1.parse::<i32>().unwrap(), part2.parse::<i32>().unwrap()))
+        .unzip();
 
     vec1.sort();
     vec2.sort();
 
-    let sum_diff: i32 = vec1.iter().zip(&vec2).map(|(a, b)| (a - b).abs()).sum();
-    println!("Sum of differences: {}", sum_diff);
+    vec1.iter().zip(&vec2).map(|(a, b)| (a - b).abs()).sum()
+}
+
+#[test]
+fn test_day1a() {
+    assert_eq!(day1a(), 1110981);
+}
+
+fn day1b() -> i32 {
+    let (vec1, vec2): (Vec<_>, Vec<_>) = INPUT
+        .lines()
+        .map(|line| line.split_once("   ").unwrap())
+        .map(|(part1, part2)| (part1.parse::<i32>().unwrap(), part2.parse::<i32>().unwrap()))
+        .unzip();
+
+    let mut counts = HashMap::new();
+
+    for num in &vec2 {
+        *counts.entry(num).or_insert(0) += 1;
+    }
+
+    let mut sum = 0;
+    for num in &vec1 {
+        if let Some(count) = counts.get(num) {
+            sum += num * count;
+        }
+    }
+    sum
+}
+
+#[test]
+fn test_day1b() {
+    assert_eq!(day1b(), 24869388);
 }
 
 fn main() {
-    day1a();
+    let sum = day1a();
+    let similarities = day1b();
+    println!("day1: {}, day2: {}", sum, similarities);
 }
